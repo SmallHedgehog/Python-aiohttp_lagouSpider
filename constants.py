f37e5d8d41442c061c24caeae3ee05c8bf0a7b31
caifei yang <caifei yang>
2017-02-11 18:36:06 +0800

    define constant values

---
+++ constants.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+""" 定义常量 """
+
+# Http请求headers
+headers = {
+    "X-Requested-With": 'XMLHttpRequest',
+    "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.89 Safari/537.36',
+    "Origin": 'https://www.lagou.com',
+    "Cookie": 'user_trace_token=20170211115515-2db01e4efbb24178989f2b6139d3698e; LGUID=20170211115515-e593a6c4-f00d-11e6-8f71-5254005c3644; showExpriedIndex=1; showExpriedCompanyHome=1; showExpriedMyPublish=1; hasDeliver=0; index_location_city=%E5%85%A8%E5%9B%BD; login=false; unick=""; _putrc=""; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1486785316; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1486789519; _ga=GA1.2.1374329991.1486785316; LGRID=20170211130519-af0ec03c-f017-11e6-a32c-525400f775ce; TG-TRACK-CODE=search_code; JSESSIONID=A5AC6E7C54130E13C1519ABA7F70BC3C; SEARCH_ID=053c985ab53e463eb5f747658872ef29',
+    "Connection": 'keep-alive'
+}
+
+# 需要获取招聘信息的职位关键字
+key_name = [
+    "Python",
+    "C++"
+]
